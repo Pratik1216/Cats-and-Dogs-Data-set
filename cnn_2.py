8b57cf9fbe7fc8b895c0f360efc2131eb4124180
Pratik1216 <58821073+Pratik1216@users.noreply.github.com>
2020-03-07 19:07:18 +0530

    Update and rename cnn.py to cnn_2.py

--- cnn_2.py
+++ cnn_2.py
@@ -1,15 +1,4 @@
-# Convolutional Neural Network
-
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# pip install tensorflow
-
-# Installing Keras
-# pip install --upgrade keras
-
-# Part 1 - Building the CNN
+# Part 2 - Building the CNN (More Complex with Incresed Number of Conv, Hidden Layers etc.)
 
 # Importing the Keras libraries and packages
 from keras.models import Sequential
@@ -22,20 +11,28 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Step 1 - Convolution
-classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(Conv2D(32, (3, 3), input_shape = (128, 128, 3), activation = 'relu')) # Changed the Input size from (32,32) to (128,128)
 
 # Step 2 - Pooling
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))                          # 2x2 is optimal
 
 # Adding a second convolutional layer
 classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
+# Adding a third convolutional layer                                      # Adding a third Conv Layer and Maxpool Layer
+classifier.add(Conv2D(64, (3, 3), activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
 # Step 3 - Flattening
 classifier.add(Flatten())
 
 # Step 4 - Full connection
-classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 64, activation = 'relu'))                    # Changing the number of units to 64 
+classifier.add(Dropout(0.6))                                              # Dropout Some Unwanted Nodes 
+classifier.add(Dense(64, activation='relu'))                              # Increasing the number of Hidden Layers
+classifier.add(Dense(64, activation='relu'))
+classifier.add(Dropout(0.3))
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
 # Compiling the CNN
@@ -53,17 +50,17 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
 training_set = train_datagen.flow_from_directory('dataset/training_set',
-                                                 target_size = (64, 64),
+                                                 target_size = (128, 128),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
 test_set = test_datagen.flow_from_directory('dataset/test_set',
-                                            target_size = (64, 64),
+                                            target_size = (128, 128),
                                             batch_size = 32,
                                             class_mode = 'binary')
 
 classifier.fit_generator(training_set,
-                         steps_per_epoch = 8000,
-                         epochs = 25,
+                         steps_per_epoch = 8000/32,
+                         epochs = 90,
                          validation_data = test_set,
-                         validation_steps = 2000)
+                         validation_steps = 2000/32)
